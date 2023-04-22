dd9123ad892f2547b3f16164f377c9c5db62390f
reikrom <mr.mygo@gmail.com>
2023-04-22 10:40:41 +0300

    chore: rename json file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const json = require(path.join(__dirname, `./src/tutor.json`))
+const json = require(path.join(__dirname, `./src/json.json`))
 
-fs.writeFileSync('./minified/tutor.json', JSON.stringify(json))
+fs.writeFileSync('./minified/json.json', JSON.stringify(json))
